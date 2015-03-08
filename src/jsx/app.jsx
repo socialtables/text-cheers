143782f5151827d3ae963c8f6b128c85c4449ec8
conorhastings <conor@socialtables.com>
2015-03-08 14:28:21 -0400

    add instructions for how to use the app

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -42,6 +42,21 @@ var App = React.createClass({
 
         <RaisedButton label="Add Your Token!" secondary={true} onClick={this.addToken} />
       	</div>
+        <div className="col-md-6 col-md-offset-3">
+           <h1>How To Use</h1>
+          <ol>
+            <li> Click on your latest TinyPulse response button in your gmail account <img src="./respondnow.png" /></li>
+            <li>When on TinyPulse, look in the url bar and copy your token, found like this: <img src="./url-screen.png" /></li>
+            <li>Enter your phone number(no spaces, dashes, etc.. - 2222222222) and paste this token above</li>
+            <li>Now you can send a cheers by texting (516)2210-4262 in the following manner, Start the text with the word cheers,
+            followed by the beginning of the persons ST email adress (conor for conor@socialtables, danmac for danmac@socialtables.com etc...)
+            </li>
+            <li>Next simply follow that up with the content of your cheers message</li>
+            <li>If you wish to make your cheers anonymous, you can make the final word of the text message "anon"</li>
+            <li>That's it</li>
+          </ol>
+
+        </div>
     	</div>
 
     );
